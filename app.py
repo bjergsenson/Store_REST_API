3d1e020ff4c655de4e4f00618885114334b53eac
Manuel Londono <mlond@softserrveinc.com>
2024-09-18 11:45:49 -0500

    Change get /stores endpoint

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.get("/store") # http://127.0.0.1:5000/store
 def get_stores():
-    return{"stores" : stores}
+    return{"stores" : list(stores.values())}
 
 @app.post("/store") # http://127.0.0.1:5000/store
 def create_store():
