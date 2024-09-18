85ba047e24c094f26bee5a4add06fa1f5f2ca8ea
Manuel Londono <mlond@softserrveinc.com>
2024-09-18 12:16:34 -0500

    Add smorest abort library to implement error handling

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import items, stores
 
 app = Flask(__name__)
@@ -25,7 +26,7 @@ def create_store():
 def create_item():
     item_data= request.get_json()
     if item_data["store_id"] not in stores:
-        return {"message", "Store not found"}, 404
+        abort(404, message= "Store not found.")
     item_id =  uuid.uuid4().hex 
     item = {**item_data, "id": item_id}
     items[item_id] = item
@@ -36,11 +37,11 @@ def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message= "Store not found.")
 
 @app.get("/item/<string:item_id>")    # http://127.0.0.1:5000/item/item_id
 def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Item not found"}, 404
+        abort(404, message= "Item not found.")
