ef5e83b200827cd0ef51356b24d226aeaacf7798
Manuel Londono <mlond@softserrveinc.com>
2024-09-18 11:33:13 -0500

    Improve data model

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
 from flask import Flask, request
+from db import items, stores
 
 app = Flask(__name__)
 
-stores = [
-    {
-        "name" : "My Store",
-        "items" : [
-            { 
-                "name" : "Chair",
-                "price" : 15.99
-            }
-        ]
-    }
-]
 
 @app.get("/store") # http://127.0.0.1:5000/store
 def get_stores():
