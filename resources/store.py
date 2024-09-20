09fcdaaeb733601bf0ee2918622ef36107235093
Manuel Londono <mlond@softserrveinc.com>
2024-09-20 13:05:14 -0500

    Implement Blueprint and MethodView to the API. Now we have a Swagger documentation

---
+++ resources/store.py
@@ -0,0 +1,40 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+
+from db import stores
+
+blp = Blueprint("stores", __name__, description = "Operations on stores")
+
+@blp.route("/store/<string:store_id>")
+class Stores(MethodView):
+    def get(self, store_id):  # http://127.0.0.1:5000/store/<store_id>
+        try:
+            return stores[store_id]
+        except KeyError:
+            abort(404, message= "Store not found.")
+
+    def delete(self, store_id):  # http://127.0.0.1:5000/store/store_id
+        try:
+            del stores[store_id]
+            return {"message" : "Store deleted."}
+        except KeyError:
+            abort(404, message= "Store not found.")
+
+@blp.reout("/store")
+class StoreList(MethodView):
+    def get(self): # http://127.0.0.1:5000/store
+        return{"stores" : list(stores.values())}
+
+    def post(self): # http://127.0.0.1:5000/store
+        store_data= request.get_json()
+        if "name" not in store_data:
+            abort(400, message = "Bad request. Ensure 'name' is included in the JSON payload.")
+        for store in stores.values():
+            if store_data["name"] == store["name"]:
+                abort(400, message = f"Store already exists.")
+        store_id =  uuid.uuid4().hex 
+        store = {**store_data, "id": store_id}
+        stores[store_id] = store
+        return store, 201
