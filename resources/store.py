6f4ae0f8f36d687fac6003f13537f56e7cc6d756
Manuel Londono <mlond@softserrveinc.com>
2024-09-20 13:31:51 -0500

    Implement marshmallow schemas to client requests

--- resources/store.py
+++ resources/store.py
@@ -4,6 +4,7 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
 from db import stores
+from schemas import StoreSchema
 
 blp = Blueprint("stores", __name__, description = "Operations on stores")
 
@@ -22,19 +23,19 @@ class Stores(MethodView):
         except KeyError:
             abort(404, message= "Store not found.")
 
-@blp.reout("/store")
+@blp.route("/store")
 class StoreList(MethodView):
     def get(self): # http://127.0.0.1:5000/store
         return{"stores" : list(stores.values())}
 
-    def post(self): # http://127.0.0.1:5000/store
-        store_data= request.get_json()
-        if "name" not in store_data:
-            abort(400, message = "Bad request. Ensure 'name' is included in the JSON payload.")
-        for store in stores.values():
-            if store_data["name"] == store["name"]:
-                abort(400, message = f"Store already exists.")
-        store_id =  uuid.uuid4().hex 
-        store = {**store_data, "id": store_id}
-        stores[store_id] = store
-        return store, 201
+    @blp.arguments(StoreSchema)
+    def post(self, store_data):
+            for store in stores.values():
+                if store_data["name"] == store["name"]:
+                    abort(400, message=f"Store already exists.")
+
+            store_id = uuid.uuid4().hex
+            store = {**store_data, "id": store_id}
+            stores[store_id] = store
+
+            return store
